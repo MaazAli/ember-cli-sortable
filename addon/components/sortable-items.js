a50b84e2611f9e6b74358388a2b7ca12ef0d8c30
Maaz Ali <maaz_ali@outlook.com>
2015-04-14 15:04:35 -0400

    Add ember-cli-htmlbars to dependencies

---
+++ addon/components/sortable-items.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+/* global Sortable */
+
+var SortableItems = Ember.Component.extend({
+  tagName: "ul",
+  classNames: ['sortable-items']
+  setup: function() {
+    var sortable = new Sortable(this.$(), {
+      animation: 150,
+      handle: this.get('handle'),
+      filter: this.get('filterClass'),
+      draggable: this.get('draggableClass'),
+      ghostClass: this.get('ghostClass')
+    });
+
+    debugger;
+  }.on('didInsertElement'),
+
+  teardown: function() {
+
+  }.on('willDestroyElement')
+
+});
+
+export default SortableItems;
