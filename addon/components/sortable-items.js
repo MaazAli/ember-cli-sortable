ddca98b21feb329d073611f7c7e0d305b1a3c810
Maaz Ali <maaz_ali@outlook.com>
2015-04-15 17:15:33 -0400

    Support all callbacks from Sortable and add more comments

--- addon/components/sortable-items.js
+++ addon/components/sortable-items.js
@@ -9,7 +9,12 @@ var SortableItems = Ember.Component.extend({
   classNames: ['sortable-items'],
   classNameBindings: ['class'],
 
-  // Allowed properties
+  /**
+    Sortable properties with reasonable defaults
+
+    @properties
+    @public
+  */
   sort: true,
   disabled: false,
   store: null,
@@ -22,6 +27,11 @@ var SortableItems = Ember.Component.extend({
   scrollSensitivity: 30, // px
   scrollSpeed: 10, // px
 
+  /**
+    @method setup
+    Initializes Sortable with given properties and binds
+    callbacks to private methods
+  */
   setup: function() {
     var self = this;
     var options = {
@@ -35,8 +45,13 @@ var SortableItems = Ember.Component.extend({
       scroll: this.get('scroll'),
       scrollSensitivity: this.get('scrollSensitivity'),
       scrollSpeed: this.get('scrollSpeed'),
-      onUpdate: Ember.run.bind(this, this._onUpdate)
-
+      onStart: Ember.run.bind(this, this._onStart),
+      onEnd: Ember.run.bind(this, this._onEnd),
+      onAdd: Ember.run.bind(this, this._onAdd),
+      onUpdate: Ember.run.bind(this, this._onUpdate),
+      onSort: Ember.run.bind(this, this._onSort),
+      onRemove: Ember.run.bind(this, this._onRemove),
+      onFilter: Ember.run.bind(this, this._onFilter)
     }
 
     if (this.get('draggable')) {
@@ -47,14 +62,43 @@ var SortableItems = Ember.Component.extend({
 
   }.on('didInsertElement'),
 
+
+  /**
+    @method _onStart
+    @private
+    The user has started to drag an item
+  */
+  _onStart: function(evt) {
+    this._sendOutAction('onStartAction', evt)
+  },
+
+  /**
+    @method _onEnd
+    @private
+    The user has stopped draggging an item
+  */
+  _onEnd: function(evt) {
+    this._sendOutAction('onEndAction', evt)
+  },
+
+  /**
+    @method _onAdd
+    @private
+    An item is dropped into the list from another list
+  */
+  _onAdd: function(evt) {
+    this._sendOutAction('onAddAction', evt)
+  },
+
   /**
     @method _onUpdate
     @private
     Changed sorting within list
   */
   _onUpdate: function(evt) {
+    this._sendOutAction('onUpdateAction', evt);
+
     Ember.run(this, function() {
-      var elements = this.$().children().clone();
       var collection = this.get('itemCollection');
       var item = collection.objectAt(evt.oldIndex);
       collection.removeObject(item);
@@ -64,11 +108,46 @@ var SortableItems = Ember.Component.extend({
         // extra item that's created because of the update
         // to the collection
         this.$().children()[evt.newIndex + 1].remove();
-        this.sendAction('onUpdateAction', item, evt.oldIndex, evt.newIndex);
+        this.sendAction('onItemMoveAction', item, evt.oldIndex, evt.newIndex);
       });
     });
   },
 
+  /**
+    @method _onSort
+    @private
+    Called when any change occurs within the list (add, update, remove)
+  */
+  _onSort: function(evt) {
+    this._sendOutAction('onSortAction', evt)
+  },
+
+
+  /**
+    @method _onRemove
+    @private
+    An item is removed from the list and added into another
+  */
+  _onRemove: function(evt) {
+    this._sendOutAction('onRemoveAction', evt)
+  },
+
+  /**
+    @method _onFilter
+    @private
+    Called when an attempt is made to drag a filtered item
+  */
+  _onFilter: function(evt) {
+    this._sendOutAction('onFilterAction', evt)
+  },
+
+
+  _sendOutAction: function(action, evt) {
+    if (this.get(action)) {
+      this.sendAction(action, evt);
+    }
+  },
+  
   teardown: function() {
     var _sortableInstance = this.get('_sortableInstance');
     if (_sortableInstance) {
