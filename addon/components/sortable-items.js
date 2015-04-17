16cebe401b52b6468715f7d7c7c165e81f99d74c
Maaz Ali <maaz_ali@outlook.com>
2015-04-16 20:58:54 -0400

    Add ability to freeze items in a list

--- addon/components/sortable-items.js
+++ addon/components/sortable-items.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 import layout from '../templates/components/sortable-items';
 /* global Sortable */
 
+var pid,
+    frozen,
+    frozenObjects,
+    positions;
+
 var SortableItems = Ember.Component.extend({
   target: Ember.computed.alias('targetObject'), // Bubble up all actions
   layout: layout,
@@ -51,7 +56,8 @@ var SortableItems = Ember.Component.extend({
       onUpdate: Ember.run.bind(this, this._onUpdate),
       onSort: Ember.run.bind(this, this._onSort),
       onRemove: Ember.run.bind(this, this._onRemove),
-      onFilter: Ember.run.bind(this, this._onFilter)
+      onFilter: Ember.run.bind(this, this._onFilter),
+      onMove: Ember.run.bind(this, this._onMove)
     }
 
     if (this.get('draggable')) {
@@ -69,7 +75,27 @@ var SortableItems = Ember.Component.extend({
     The user has started to drag an item
   */
   _onStart: function(evt) {
-    this._sendOutAction('onStartAction', evt)
+
+    Ember.run(this, function() {
+      var freezeSelector = this.get('freeze');
+
+      if (freezeSelector) {
+        var _sortableInstance = this.get('_sortableInstance');
+        var itemCollection = this.get('itemCollection');
+        frozenObjects = [];
+
+        frozen = [].slice.call(_sortableInstance.el.querySelectorAll(freezeSelector));
+        positions = frozen.map(function (el) {
+          return Sortable.utils.index(el);
+        });
+
+        positions.forEach(function(position) {
+          frozenObjects.pushObject(itemCollection.objectAt(position));
+        });
+      }
+    });
+
+    this._sendOutAction('onStartAction', evt);
   },
 
   /**
@@ -78,7 +104,7 @@ var SortableItems = Ember.Component.extend({
     The user has stopped draggging an item
   */
   _onEnd: function(evt) {
-    this._sendOutAction('onEndAction', evt)
+    this._sendOutAction('onEndAction', evt);
   },
 
   /**
@@ -87,7 +113,7 @@ var SortableItems = Ember.Component.extend({
     An item is dropped into the list from another list
   */
   _onAdd: function(evt) {
-    this._sendOutAction('onAddAction', evt)
+    this._sendOutAction('onAddAction', evt);
   },
 
   /**
@@ -101,15 +127,33 @@ var SortableItems = Ember.Component.extend({
     Ember.run(this, function() {
       var collection = this.get('itemCollection');
       var item = collection.objectAt(evt.oldIndex);
-      collection.removeObject(item);
+      var list = evt.to;
+      var freezeSelector = this.get('freeze');
+      collection.removeAt(evt.oldIndex);
       collection.insertAt(evt.newIndex, item);
-      Ember.run.scheduleOnce('afterRender', this, function() {
-        // After the render we'll have to remove the
-        // extra item that's created because of the update
-        // to the collection
-        this.$().children()[evt.newIndex + 1].remove();
-        this.sendAction('onItemMoveAction', item, evt.oldIndex, evt.newIndex);
-      });
+      // Remove the duplicate
+      evt.item.parentNode.removeChild(evt.item);
+
+      if (freezeSelector) {
+        frozenObjects.forEach(function(obj, i) {
+          var pos = positions[i];
+          if (collection.objectAt(pos) !== obj) {
+            var realPos = collection.indexOf(obj);
+            collection.removeAt(realPos);
+            collection.insertAt(pos, obj);
+            // Remove the duplicate
+            if (realPos > pos) {
+              list.children[pos - 1].remove();
+            } else {
+              list.children[pos].remove();
+            }
+          }
+        });
+      }
+
+
+      this.sendAction('onItemMoveAction', item, evt.oldIndex, evt.newIndex);
+
     });
   },
 
@@ -119,7 +163,7 @@ var SortableItems = Ember.Component.extend({
     Called when any change occurs within the list (add, update, remove)
   */
   _onSort: function(evt) {
-    this._sendOutAction('onSortAction', evt)
+    this._sendOutAction('onSortAction', evt);
   },
 
 
@@ -129,7 +173,7 @@ var SortableItems = Ember.Component.extend({
     An item is removed from the list and added into another
   */
   _onRemove: function(evt) {
-    this._sendOutAction('onRemoveAction', evt)
+    this._sendOutAction('onRemoveAction', evt);
   },
 
   /**
@@ -138,16 +182,65 @@ var SortableItems = Ember.Component.extend({
     Called when an attempt is made to drag a filtered item
   */
   _onFilter: function(evt) {
-    this._sendOutAction('onFilterAction', evt)
+    this._sendOutAction('onFilterAction', evt);
+  },
+
+  /**
+    @method _onMove
+    @private
+    Called when re-ordering the list during drag
+  */
+  _onMove: function(evt) {
+    this._sendOutAction('onMoveAction', evt);
+
+    Ember.run(this, function() {
+      var vector;
+      var freeze = false;
+      var freezeSelector = this.get('freeze');
+
+      if (freezeSelector) {
+        clearTimeout(pid);
+
+        pid = setTimeout(function () {
+          var list = evt.to;
+          frozen.forEach(function (el, i) {
+            var idx = positions[i];
+
+            if (list.children[idx] !== el) {
+              var realIdx = Sortable.utils.index(el);
+              list.insertBefore(el, list.children[idx + (realIdx < idx)]);
+            }
+          });
+        }, 0);
+
+        frozen.forEach(function (el, i) {
+          if (el === evt.related) {
+            freeze = true;
+          }
+
+          if (evt.related.nextElementSibling === el &&
+              evt.relatedRect.top < evt.draggedRect.top) {
+            vector = -1;
+          }
+        });
+        return freeze ? false : vector;
+      }
+    });
   },
 
 
+  /**
+    @method _sendOutAction
+    @private
+    Used as an interface for the sendAction method, checks if consumer defined
+    an action before sending one out
+  */
   _sendOutAction: function(action, evt) {
     if (this.get(action)) {
       this.sendAction(action, evt);
     }
   },
-  
+
   teardown: function() {
     var _sortableInstance = this.get('_sortableInstance');
     if (_sortableInstance) {
